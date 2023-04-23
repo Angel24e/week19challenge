66d3c2058dfe7882add00e4d080d2414b568e290
angel Escamilla <angel4esc@gmail.com>
2023-04-22 17:38:12 -0700

    done

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -22,7 +22,7 @@ module.exports = () => {
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: './dist/index.html',
+        template: './index.html',
         title: 'J.A.T.E'
       }),
       new InjectManifest({
